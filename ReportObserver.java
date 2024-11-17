50f1e40eddac124a2b76d0301757362e7fb9e355
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-11-16 22:30:01 -0400

    Delete ReportObserver.java

--- ReportObserver.java
+++
@@ -1,9 +0,0 @@
-//observer interface
-package project;
-
-import java.io.IOException;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-
-public interface ReportObserver {
-    void update(PDPageContentStream contentStream) throws IOException;
-}
