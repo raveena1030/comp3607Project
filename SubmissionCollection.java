b1e99267657a32f24ed63304d87da9a97d07d034
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-11-16 22:30:12 -0400

    Delete SubmissionCollection.java

--- SubmissionCollection.java
+++
@@ -1,9 +0,0 @@
-package comp3607project.ZipFileHandling;
-
-import java.io.File;
-
-//Aggregate
-public interface SubmissionCollection {
-    void addSubmission (File file);
-    SubmissionIterator createFileIterator();
-}
